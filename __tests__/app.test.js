63a19abb316484b616a746e043b786a7d86bd68c
James <jameswilson8@hotmail.co.uk>
2025-01-27 12:28:38 +0000

    set up app.js file and create /api end point with the correct endpoints data imported

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,11 +1,22 @@
 const endpointsJson = require("../endpoints.json");
 const request = require("supertest");
 const app = require("../app");
+const testData = require("../db/data/test-data/index");
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
 /* Set up your test imports here */
 
 /* Set up your beforeEach & afterAll functions here */
 
-describe.skip("GET /api", () => {
+beforeEach(() => {
+  return seed(testData);
+});
+
+afterAll(() => {
+  return db.end();
+});
+
+describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
     return request(app)
       .get("/api")
--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const endpointsJson = require("./endpoints.json");
 const app = express();
+
+app.get("/api", (req, res) => {
+  res.status(200).send({ endpoints: endpointsJson });
+});
 
 module.exports = app;
